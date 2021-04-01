fcb80109458c28f9a94ad8fb45836bc1a6a24fad
Veera0742 <veera.lumioksa@helsinki.fi>
2021-04-01 13:48:00 +0300

    Changes to the code

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 from flask import Flask
 from flask import redirect, render_template, request, session
+from flask_sqlalchemy import SQLAlchemy
 from os import getenv
 from werkzeug.security import check_password_hash, generate_password_hash
 
 app = Flask(__name__)
 app.secret_key = getenv("SECRET_KEY")
 app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
-#db = SQLAlchemy(app)
+db = SQLAlchemy(app)
 
 @app.route("/")
 def index():
