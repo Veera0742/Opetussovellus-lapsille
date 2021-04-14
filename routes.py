a78f837b5ec71dee3b1538e2fd2147f850ebb40e
Veera0742 <veera.lumioksa@helsinki.fi>
2021-04-14 13:42:39 +0300

    Changes to code and templates

--- routes.py
+++ routes.py
@@ -1,27 +1,43 @@
 from app import app
-from flask import render_template, request, session, redirect
+from flask import Flask, render_template, request, session, redirect
+import users
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
-@app.route("/register")
+@app.route("/login", methods=["get","post"])
+def login():
+    if request.method == "GET":
+        return render_template("login.html")
+
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        if users.login(username, password):
+            return redirect("/")
+        else:
+            return render_template("mistake.html")
+
+@app.route("/register")# methods=["POST"])
 def register():
     return render_template("register.html")
-    if users.register(username, password):
-        return redirect("/")
-    else:
-        return render_template("mistake.html")
+#
+#    username = request.form["username"]
+ #   if len(username) < 1 or len(username) > 20:
+  #      return render_template("mistake.html")
 
-@app.route("/login")
-def login():
-    return render_template("login.html")
-    username = request.form["username"]
-    password = request.form["password"]
-    if users.login(username, password):
-        return redirect("/start")
-    else: 
-        return render_template("mistake.html")
+#    password1 = request.form["password1"]
+#    password2 = request.form["password2"]
+#    if password1 != password2:
+#        return render_template("mistake.html")
+#    if password1 == "":
+#        return render_template("mistake.html")
+
+#    if users.register(username, password1):
+#        return redirect("/login")
+#    else:
+#        return render_template("mistake.html")
 
 @app.route("/start")
 def start():
--- users.py
+++ users.py
@@ -1,22 +1,24 @@
 from db import db
 from flask import session, request
 from werkzeug.security import check_password_hash, generate_password_hash
+import os
 
-def register(username, password): 
+def register(username, password):
     hash_value = generate_password_hash(password)
-    sql = "INSERT INTO users (username,password) VALUES (:username,:password)"
-    db.session.execute(sql, {"username":username,"password":hash_value})
+    sql = "INSERT INTO users (username, password) VALUES (:username, :password)"
+    db.session.execute(sql, {"username":username, "password":hash_value})
     db.session.commit()
+    return redirect("/")
 
 def login(username, password):
-    sql = "SELECT password FROM users WHERE username=:username"
+    sql = "SELECT password, id FROM users WHERE username=:username"
     result = db.session.execute(sql, {"username":username})
-    user = result.fetchone()    
+    user = result.fetchone()   
     if user == None:
-        print("Väärä tunnus")
+        return redirect("/login")
     else:
         hash_value = user[0]
         if check_password_hash(hash_value,password):
-            print("Oikea tunnus ja salasana")
+            return redirect("/start")    
         else:
-            print("Väärä salasana")
+            return render_template("mistake.html")
